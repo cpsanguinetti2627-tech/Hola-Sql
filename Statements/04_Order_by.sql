753aa64cc546b499146799c425be2712f06a3908
cpsanguinetti2627-tech <cpsanguinetti2627@gmail.com>
2026-06-07 15:47:17 -0400

    Update 04_Order_by.sql
    Muestra el email hola123@gmail.com ordenado por edad de forma descendente de la tabla users

--- Statements/04_Order_by.sql
+++ Statements/04_Order_by.sql
@@ -1,3 +1,4 @@
 SELECT * FROM users ORDER BY age
 SELECT * FROM users ORDER BY age DESC
 SELECT * FROM users ORDER BY age ASC
+SELECT * FROM users WHERE email = "hola123@gmail.com" ORDER BY age DESC
