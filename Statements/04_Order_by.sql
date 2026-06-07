4a4edbcb2d8527e97bc563eedb88d50b66dcd39c
cpsanguinetti2627-tech <cpsanguinetti2627@gmail.com>
2026-06-07 15:50:05 -0400

    Update 04_Order_by.sql
    Muestra los email hola123@gamilc.om por los nombres ordenados de forma ascendente de a tabla Users

--- Statements/04_Order_by.sql
+++ Statements/04_Order_by.sql
@@ -3,3 +3,4 @@ SELECT * FROM users ORDER BY age DESC
 SELECT * FROM users ORDER BY age ASC
 SELECT * FROM users WHERE email = "hola123@gmail.com" ORDER BY age DESC
 SELECT * FROM users WHERE email = "hola123@gmail.com" ORDER BY age ASC
+SELECT name FROM users WHERE email = "hola123@gmail.com" ORDER BY age DESC
